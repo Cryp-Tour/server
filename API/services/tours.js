39b3985193f3cebea0b74ab98394827075e4de2b
Victoria Strobel <victoria.strobel@dhbw-engineering.de>
2022-04-14 14:58:42 +0200

    removed unnecessary comments

--- API/services/tours.js
+++ API/services/tours.js
@@ -91,22 +91,6 @@ module.exports.createTour = async (options) => {
  * @return {Promise}
  */
 module.exports.getTour = async (options) => {
-  // Implement your business logic here...
-  //
-  // This function should return as follows:
-  //
-  // return {
-  //   status: 200, // Or another success code.
-  //   data: [] // Optional. You can put whatever you want here.
-  // };
-  //
-  // If an error happens during your business logic implementation,
-  // you should throw an error as follows:
-  //
-  // throw new ServerError({
-  //   status: 500, // Or another error code.
-  //   error: 'Server Error' // Or another error message.
-  // });
   await dao.get(
     `SELECT tID, title, difficulty, location, distance, duration, description, creatorID FROM tour WHERE tID = ?`, 
     [options.TID]
