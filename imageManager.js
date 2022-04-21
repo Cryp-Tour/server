743c9e5389f3bef22b48fe4a87d7351c2f42cc8b
David Ullmer <davidullmer@outlook.de>
2022-04-21 14:47:37 +0200

    Get image for .jpeg, .png and .svg support

--- imageManager.js
+++ imageManager.js
@@ -1,7 +1,37 @@
-module.exports.getImagePath = (tID, imageID) => {
+const mime = require('mime-types');
+const fs = require('fs');
+
+/**
+ * Get image path for tour id and image id
+ * @param {int} tID the id of the tour
+ * @param {int} imageID the id of the image
+ * @param {string} mimetype optional. used for uploading. if passed, image with extension according to mimetype is created. if not passed, there will be a search for all extensions
+ * @returns the image path
+ */
+module.exports.getImagePath = (tID, imageID, mimetype = "") => {
     // return different path if server is running inside docker
-    if (process.env.IN_DOCKER == 1){
-        return `/data/${tID}/${imageID}.jpg`;
+    var prefix = "";
+    if (process.env.IN_DOCKER != 1){
+        prefix = __dirname;
     }
-    return __dirname + `/data/${tID}/${imageID}.jpg`;
+
+    var path;
+    var supported_extensions = ["jpg", "jpeg", "png", "svg"];
+    // get path with mimetype
+    if (mimetype) {
+        var extension = mime.extension(mimetype);
+        path = `${prefix}/data/${tID}/${imageID}.${extension}`;
+    } else {
+        supported_extensions.every(extension => {
+            path = `${prefix}/data/${tID}/${imageID}.${extension}`
+            if (fs.existsSync(path)) {
+                // returning false will stop the "every" loop
+                // see: https://masteringjs.io/tutorials/fundamentals/foreach-break
+                return false;
+            };
+            return true;
+        });
+    }
+
+    return path;
 };
